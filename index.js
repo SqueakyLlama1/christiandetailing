6c64c1ac268a840ec91d12cf79ce4707f64ef836
SqueakyLlama1 <christian.c.bone@gmail.com>
2025-04-08 09:37:08 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function navigate(url) {
+	document.getElementById('loadshade').style.display = "block";
+	const universe = "https://universe-os.com/";
+	if (url == 'home') {
+		window.location.href = universe;
+	} else {
+		window.location.href = universe + url;
+	}
+}
+
+function applyNavigation() {
+	const navcontent = `
+	<button onclick="navigate('home')">Home</button><br>
+	<button onclick="navigate('download/')">Download</button><br>
+ 	<button onclick="navigate('contact/')">Contact</button>`
+	document.getElementById('navigation').innerHTML = navcontent;
+}
+
+document.addEventListener('DOMContentLoaded', applyNavigation);
